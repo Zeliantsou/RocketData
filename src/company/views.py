f78f22f97b9e8e6a76f9a8a34d74eb4564ed6c75
Zeliantsou <exp89@mail.ru>
2022-11-21 15:13:36 +0300

    RD-4.3-drf Receiving companies with more than average debt was added

--- src/company/views.py
+++ src/company/views.py
@@ -1,5 +1,8 @@
+from django.db.models import Avg
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from company.models import Company
@@ -15,7 +18,16 @@ class CompanyViewSet(
     search_fields = ('country__name',)
     serializer_classes = {
         'list': CompanyListSerializer,
+        'get_with_debt_more_avg': CompanyListSerializer,
     }
+
+    @action(methods=('get',), detail=False, url_path='with-debt-more-than-average')
+    def get_with_debt_more_avg(self, request,):
+        average_debt = Company.objects.aggregate(Avg('debt'))
+        companies = Company.objects.filter(debt__gt=average_debt.get('debt__avg'))
+
+        serializer = self.get_serializer(companies, many=True)
+        return Response(serializer.data)
 
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action)
